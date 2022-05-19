7eea1af2d7bf6c1c805569a5f707e729ba9e998b
Omar Gomez <omar.gomez@gmail.com>
2022-05-19 19:27:58 +0200

    Use case added

--- MoneyRates/Domain/UseCases/ConversionUseCase.swift
+++ MoneyRates/Domain/UseCases/ConversionUseCase.swift
@@ -6,12 +6,15 @@
 //
 
 import Foundation
+import Combine
 
 protocol ConversionUseCase {
     
     typealias CompletionType = (Result<Double, Error>) -> ()
     
     func execute(sourceSymbol: String, targetSymbol: String, amount: Double, completion: @escaping CompletionType )
+    
+    func execute(sourceSymbol: String, targetSymbol: String, amount: Double) -> AnyPublisher <Double, Error>
 }
 
 final class ConversionUseCaseImpl: ConversionUseCase {
@@ -32,5 +35,26 @@ final class ConversionUseCaseImpl: ConversionUseCase {
         self.exchangeService.convert(sourceSymbol: sourceSymbol, targetSymbol: targetSymbol, amount: amount, completion: { result in
             completion(result)
         })
+    }
+    
+
+    func execute(sourceSymbol: String, targetSymbol: String, amount: Double) -> AnyPublisher <Double, Error> {
+        
+        let result: Future<Double, Error> = Future() { promise in
+            guard amount >= 0.01 else {
+                return promise(.success(0.0))
+            }
+            
+            self.exchangeService.convert(sourceSymbol: sourceSymbol, targetSymbol: targetSymbol, amount: amount, completion: { result in
+                switch result {
+                case .success(let value):
+                    return promise(.success(value))
+                case .failure(let error):
+                    return promise(.failure(error))
+                }
+            })
+        }
+        
+        return result.eraseToAnyPublisher()
     }
 }
